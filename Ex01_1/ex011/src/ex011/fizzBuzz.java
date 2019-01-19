beaeb48ca44af85314917028e9512ffe1c8517e7
宮久保昂樹 <itukichan@itukichan.local>
2019-01-19 17:34:51 +0900

    comment delete

--- Ex01_1/ex011/src/ex011/fizzBuzz.java
+++ Ex01_1/ex011/src/ex011/fizzBuzz.java
@@ -3,7 +3,6 @@ package ex011;
 public class fizzBuzz {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		for(int i = 1; i<=100; i++) {
 			if((i % 3 == 0) && (i % 5 == 0)) {
 				System.out.println("FizzBuzz");
